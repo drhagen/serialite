4d32051087e31386a9e06857d0bc4fd8dfdcc290
David Hagen <david@drhagen.com>
2025-11-09 08:35:29 -0500

    Path

--- src/serialite/_implementations/_path.py
+++ src/serialite/_implementations/_path.py
@@ -3,17 +3,17 @@ __all__ = ["PathSerializer"]
 from pathlib import Path
 
 from .._base import Serializer
+from .._errors import Errors
 from .._result import Failure, Result, Success
-from ._string import StringSerializer
+from ._type_errors import ExpectedStringError
 
 
 class PathSerializer(Serializer):
     def from_data(self, data) -> Result[Path]:
-        match StringSerializer().from_data(data):
-            case Failure(error):
-                return Failure(error)
-            case Success(value):
-                return Success(Path(value))
+        if not isinstance(data, str):
+            return Failure(Errors.one(ExpectedStringError(data)))
+
+        return Success(Path(data))
 
     def to_data(self, value):
         if not isinstance(value, Path):
