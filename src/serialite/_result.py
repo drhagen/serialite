f8f7be6c84589cc5ad8b48e032eb0187196bce05
Renan Rodrigues dos Santos <renan.engmec@gmail.com>
2026-03-31 19:53:30 -0300

    Remove type annotations not needed anymore

--- src/serialite/_result.py
+++ src/serialite/_result.py
@@ -2,20 +2,11 @@ from __future__ import annotations
 
 __all__ = ["Failure", "Result", "Success"]
 
-from typing import TYPE_CHECKING
-
 from returns import result
 
 from ._errors import Errors
 
-if TYPE_CHECKING:
-    # TODO: remove this when https://github.com/astral-sh/ty/issues/136 is resolved
-    # See https://github.com/astral-sh/ty/issues/2788
-    class Result[Output]:
-        def unwrap(self) -> Output: ...
-        def failure(self) -> Errors: ...
-else:
-    type Result[Output] = result.Result[Output, Errors]
+type Result[Output] = result.Result[Output, Errors]
 
 Success = result.Success
 Failure: type[result.Failure[Errors]] = result.Failure[Errors]
